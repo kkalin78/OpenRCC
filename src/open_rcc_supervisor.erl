09cc5d04a6095fa755e60224da66d887b939fe0a
Konstantin Kalin <konstantin.kalin@gmail.com>
2012-05-12 23:16:40 -0700

    Cleanup open_rcc_server code. Reimplement login and logout methods according to OpenACD v1 codebase

--- src/open_rcc_supervisor.erl
+++ src/open_rcc_supervisor.erl
@@ -9,6 +9,7 @@ start_link() ->
     supervisor:start_link(open_rcc_supervisor, []).
 
 init(_Args) ->
+    % TODO - Make Port configureable
     {ok, {{one_for_one, 1, 60},
           [{open_rcc, {open_rcc_server, start_link, [8383]},
             permanent, brutal_kill, worker, [open_rcc]}]}}.
