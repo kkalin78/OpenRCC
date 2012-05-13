31514d91934b2e76255fb57b536bb2adc3057083
Konstantin Kalin <konstantin.kalin@gmail.com>
2012-05-12 21:21:05 -0700

    Fix OpenRCC OTP application. Now it starts automaticaly

--- src/open_rcc_app.erl
+++ src/open_rcc_app.erl
@@ -1,9 +1,12 @@
--module(open_rcc).
+-module(open_rcc_app).
 -behaviour(application).
+
+-include_lib("OpenACD/include/log.hrl").
 
 -export([start/2, stop/1]).
 
 start(_Type, _Args) ->
+    ?INFO("Starting OpenRCC application", []),
     open_rcc_supervisor:start_link().
 
 stop(_State) ->
